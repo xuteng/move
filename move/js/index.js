1852b81f9ba1a1995709d4b51cd2d54d882f1881
徐腾 <xutxxx@163.com>
2017-12-22 11:10:08 +0800

    Add files via upload

---
+++ move/js/index.js
@@ -0,0 +1,147 @@
+var option = {
+	on: {
+		slideChange: function() {
+			var _index = this.activeIndex;
+			switch (_index) {
+				case 0:
+					$('.wrapper').removeClass('next');
+					break;
+				case 1:
+					(function() {
+						$('.s22').addClass('active')
+						$('.s25').addClass('active')
+						$('.s27').addClass('active')
+						setTimeout(function() {
+							$('.s24').addClass('active')
+						}, 2000)
+
+					})();
+					break;
+				case 2:
+					(function() {
+						$('.s36').addClass('active')
+					})();
+					break;
+				case 3:
+					(function() {
+						$('.s41').addClass('active')
+					})();
+					break;
+				case 4:
+                    (function() {
+                        $('.s52').addClass('active')
+                    })();
+                    break;
+				case 5:
+					(function() {
+						setTimeout(function() {
+							$('.s63').addClass('active')
+						}, 500)
+					})();
+					break;
+				case 6:
+					(function() {
+						setTimeout(function() {
+							$('.s72').addClass('active')
+						}, 200)
+						setTimeout(function() {
+							$('.s73').addClass('active')
+						}, 800)
+					})();
+					break;
+				case 7:
+				(function() {
+					$('.m-text').addClass('active');
+					setTimeout(function() {
+						$('.hua:last').addClass('active')
+					}, 800)
+					$('.egg').css({
+						left: '6rem',
+						bottom: '5.46875rem'
+					}).animate({
+						left: '3.34375rem',
+						bottom: '4.6875rem',
+						width: '.8rem'
+					}, 100, function() {
+						$('.egg').animate({
+							left: '.375rem',
+							bottom: '.15625rem',
+							width: '2.421875rem'
+						}, 400)
+					});
+				})();
+					break;
+				default:
+					break
+			}
+		},
+	},
+}
+$(document).ready(function() {
+	//图片预加载
+    var _all = $(".wrapper").find("*"),_arr = [],t_img,isLoad = true,rg = new RegExp("url"),_imgs = $(".wrapper").find("img"),
+        fn = function(){ 
+        	$(".loading").addClass('hide');
+			init();
+        };
+    //TODO 环境变量
+    var _pathIndex = window.location.href.lastIndexOf('/')+1;
+
+    _all.each(function(){
+        if(rg.test($(this).css("background-image"))){
+            var _url = $(this).css("background-image");
+            var _img = new Image();
+            _url = _url.replace(/url\(|\)/g,"");
+            _url = _url.replace(/\"/g,"");
+            _img.src=_url;
+            _arr.push(_img);
+        }
+    });
+    _imgs.each(function(){
+    	//TODO 环境变量
+        var _url = window.location.href.substring(0,_pathIndex)+$(this).attr("src");
+        var _img = new Image();
+        _img.src=_url;
+        _arr.push(_img);
+    });
+    
+    function isImgLoad(callback){
+        $(_arr).each(function(){
+            if(this.width === 0){
+                isLoad = false;
+                return false;
+            }
+        });
+        if(isLoad){
+            clearTimeout(t_img);
+            callback();
+        }else{
+            isLoad = true;
+            t_img = setTimeout(function(){
+                isImgLoad(callback);
+            },1000);
+        }
+    }
+    isImgLoad(fn);
+});
+var mySwiper
+function init(){
+	$(".loading").addClass('hide');
+	mySwiper = new Swiper('.container',option)
+	document.getElementById("train").play()
+	$('.train').addClass('out');
+	$('.text-wrapper').addClass('active');
+	setTimeout(function(){
+		$('.hua:first').addClass('active')
+
+	},500)
+	setTimeout(function(){
+		document.getElementById("train").pause()
+		document.getElementById("bg").play()
+	},2000)
+	$('.egg').click(function(){
+		$('.mask').slideDown('300', function() {
+			$('.s84').addClass('active')
+		});
+	})
+}
